a06f6075ea09dee62bb5c5b79c18bc490f06467d
Renato Teixeira <rena2damas@gmail.com>
2021-03-19 21:48:56 +0000

    Add run class for PNCalculator

---
+++ java/PNCalculator/src/RunPNCalculator.java
@@ -0,0 +1,15 @@
+import java.io.IOException;
+
+
+/**
+ * The run class for PNCalculator
+ */
+public class RunPNCalculator {
+
+  /**
+   * Start the interactive program
+   */
+  public static void main(String[] args) throws IOException {
+
+  }
+}
