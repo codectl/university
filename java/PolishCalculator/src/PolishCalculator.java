826d739197c50195f0640e9f97141eb575a26010
Renato Teixeira <rena2damas@gmail.com>
2021-03-23 09:16:54 +0000

    Rename project & interctive menu

--- java/PolishCalculator/src/PolishCalculator.java
+++ java/PolishCalculator/src/PolishCalculator.java
@@ -3,104 +3,24 @@ import java.util.stream.Collectors;
 
 
 /**
- * Representation of a polish notation calculator
+ * A calculator that uses polish notation (prefix)
+ * and reverse polish notation (suffix) to solve
+ * infix algebraic expressions.
  */
-public class PNCalculator {
+public class PolishCalculator {
+
+  private final String expression;
 
   // Supported operators
   private final static Set<Character> operators =
       new HashSet<>(Arrays.asList('+', '-', '*', '/', '%'));
 
-  /*
-   *
-   */
-  public static boolean verificaParenteses(String infixa) {
-    Scanner leitor = new Scanner(infixa);
-    boolean equilibrada = true;
-    Stack<String> pilha = new Stack<String>();
-    String token = "";
-    while (equilibrada && leitor.hasNext()) {
-      token = leitor.next();
-      if (token.equals("("))
-        pilha.push(token);
-      else if (token.equals(")")) {
-        if (!pilha.isEmpty() && pilha.peek().equals("("))
-          pilha.pop();
-        else
-          equilibrada = false;
-      }
-    }
-    return (equilibrada && pilha.empty());
+  public PolishCalculator(String expression) {
+    if (!validateInfixExpression(expression))
+      throw new IllegalArgumentException("Invalid expression");
+
+    this.expression = expression;
   }
-
-  /*
-   *
-   */
-  public String paraSufixa(String infix) {
-    Scanner reader = new Scanner(infix);
-
-    Stack<String> pilha = new Stack<String>();
-    StringBuilder sufixa = new StringBuilder();
-    String token = "";
-
-//    // Read token by token
-//    while (reader.hasNext()) {
-//      token = reader.next();
-//      if ()
-//    }
-
-    return null;
-//    while (reader.hasNext()) {
-//      token = reader.next();
-//      if (!eOperador(token))
-//        sufixa.append(token + " ");
-//
-//      else if (eOperador(token)) {
-//        while (temPreferencia(pilha.peek(), token) && !pilha.isEmpty()) {
-//          sufixa.append(pilha.pop() + " ");
-//        }
-//        pilha.push(token);
-//      } else if (token.equals("("))
-//        pilha.push(token);
-//
-//      else if (token.equals(")")) {
-//        while (pilha.peek() != "(") {
-//          sufixa.append(pilha.pop() + " ");
-//        }
-//        pilha.pop();
-//      }
-//    }
-//    while (!pilha.isEmpty()) {
-//      sufixa.append(pilha.pop() + " ");
-//    }
-//    return sufixa.toString();
-  }
-
-  /*
-   *
-   */
-//  public void processToken(char token) {
-//
-//    if (isOperator(token))
-//      stack.push();
-//
-//    if (!eOperador(token))
-//      stack.push(Double.parseDouble(token));
-//    else {
-//      double a = stack.peek();
-//      stack.pop();
-//      double b = stack.peek();
-//      stack.pop();
-//      if (token.equals("+"))
-//        stack.push(a + b);
-//      else if (token.equals("-"))
-//        stack.push(a - b);
-//      else if (token.equals("*"))
-//        stack.push(a * b);
-//      else if (token.equals("/"))
-//        stack.push(a / b);
-//    }
-//  }
 
   /**
    * Check whether the given character is an arithmetic operator
@@ -114,13 +34,9 @@ public class PNCalculator {
 
   /**
    * Convert an infix expression into a suffix.
-   * @param expression the expression to convert
    * @return the suffix expression
    */
-  public static String infixToSuffix(String expression) {
-    if (!validateInfixExpression(expression))
-      throw new IllegalArgumentException("Invalid expression");
-
+  public String infixToSuffix() {
     StringBuilder postfix = new StringBuilder();
     Stack<Character> operators = new Stack<>();
 
@@ -165,17 +81,64 @@ public class PNCalculator {
   }
 
   /**
+   * Convert an infix expression into a prefix.
+   * @return the prefix expression
+   */
+  public String infixToPrefix() {
+    return new StringBuilder(infixToSuffix()).reverse().toString();
+  }
+
+  /**
+   * Solve an algebraic expression.
+   * Algorithm: 1) iterate over suffix expression.
+   *            2) if element is operand, push to the stack
+   *            3) if element is operator, perform operation
+   *               over the 2 top elements of the stack and
+   *               push result back to the stack.
+   *            4) repeat steps 1 - 3 over all the expression
+   *               and the final result is the top of the stack
+   * @return the result
+   */
+  public double solve() {
+    String suffix = infixToSuffix();
+    Stack<Double> stack = new Stack<>();
+    for (String element : suffix.split(" ")){
+      if (isOperator(element.charAt(0))) {
+        switch (element){
+          case "+":
+            stack.push(stack.pop() + stack.pop());
+            break;
+          case "-":
+            stack.push(stack.pop() - stack.pop());
+            break;
+          case "*":
+            stack.push(stack.pop() * stack.pop());
+            break;
+          case "/":
+            stack.push(stack.pop() / stack.pop());
+            break;
+          case "%":
+            stack.push(stack.pop() % stack.pop());
+            break;
+        }
+      }  else
+          stack.push(Double.valueOf(element));
+    }
+    return stack.pop();
+  }
+
+  /**
    * Check whether one operator has precedence over the other
    *
-   * @param o1 the left operator
-   * @param o2 the right operator
+   * @param c1 the left operator
+   * @param c2 the right operator
    * @return true if o1 has precedence over o2
    */
-  public static boolean hasPrecedence(char o1, char o2) {
-    if (!isOperator(o1) || !isOperator(o2))
+  private static boolean hasPrecedence(char c1, char c2) {
+    if (!isOperator(c1) || !isOperator(c2))
       throw new IllegalArgumentException("Wrong operator provided.");
 
-    return "*/%".indexOf(o1) >= 0 && "+-".indexOf(o2) >= 0;
+    return "*/%".indexOf(c1) >= 0 && "+-".indexOf(c2) >= 0;
   }
 
   /**
@@ -186,7 +149,7 @@ public class PNCalculator {
    * @param expression the expression to validate.
    * @return true if expression is correct, false otherwise.
    */
-  public static boolean validateInfixExpression(String expression) {
+  static boolean validateInfixExpression(String expression) {
     List<String> expressions;
     try {
       expressions = getBracketExpressions(expression);
@@ -228,7 +191,7 @@ public class PNCalculator {
               &&
               expressions
                   .stream()
-                  .map(PNCalculator::validateInfixExpression)
+                  .map(PolishCalculator::validateInfixExpression)
                   .reduce(true, (elem1, elem2) -> elem1 & elem2);
     }
   }
---
+++ java/PolishCalculator/src/RunPolishCalculator.java
@@ -0,0 +1,87 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+
+/**
+ * The run class for PolishCalculator
+ */
+public class RunPolishCalculator {
+
+  /**
+   * Start the interactive program
+   */
+  public static void main(String[] args) {
+
+    // user input
+    Scanner in = new Scanner(System.in);
+
+    // start the user interaction with initial instructions
+    System.out.println(
+        "\n*** Welcome to the PNCalculator ***\n" +
+            "A calculator that uses polish notation for algebraic expressions."
+    );
+    System.out.println();
+
+    PolishCalculator calculator;
+    String expression = null;
+    int option;
+
+    do {
+      do {
+        if (expression != null) {
+          System.out.println("Invalid expression. Try again ...");
+          System.out.println();
+        }
+        System.out.println("Valid operators are: + - * / %.");
+        System.out.println("Parenthesis are also supported.");
+        System.out.print("Expression: ");
+        expression = in.nextLine().trim();
+
+      } while (!PolishCalculator.validateInfixExpression(expression));
+
+      // Create new expression
+      calculator = new PolishCalculator(expression);
+      expression = null;
+
+      do {
+        System.out.println();
+        System.out.println(
+            "Choose your option:\n" +
+                "\t1) convert to suffix\n" +
+                "\t2) convert to prefix\n" +
+                "\t3) show result\n" +
+                "\t4) new expression\n" +
+                "\t5) exit"
+        );
+        System.out.print("Your option [1-5]: ");
+        option = in.nextInt();
+        in.nextLine();  // Consume newline left over
+        System.out.println();
+
+        // validate user selection
+        switch (option) {
+          case 1:
+            // convert to suffix notation
+            System.out.println("Suffix: " + calculator.infixToSuffix());
+            break;
+          case 2:
+            // convert to prefix notation
+            System.out.println("Prefix " + calculator.infixToPrefix());
+            break;
+          case 3:
+            // solve expression
+            System.out.println("Result: " + calculator.solve());
+            break;
+          case 4:
+            break;
+          case 5:
+            System.out.println("Bye.");
+            break;
+          default:
+            System.out.println("Provide a valid option.");
+            break;
+        }
+      } while (!Arrays.asList(4, 5).contains(option));
+    } while (option != 5);
+  }
+}
