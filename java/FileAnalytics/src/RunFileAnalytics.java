2d03a4eb27656b0d29595eb5ed513b43e8cb24b7
Renato Teixeira <rena2damas@gmail.com>
2021-04-08 22:32:17 +0100

    Update instructions

--- java/FileAnalytics/src/RunFileAnalytics.java
+++ java/FileAnalytics/src/RunFileAnalytics.java
@@ -17,13 +17,7 @@ public class RunFileAnalytics {
     Scanner in = new Scanner(System.in);
 
     // start the user interaction with initial instructions
-    System.out.println(
-        "\n*** Welcome to the FileAnalytics ***\n" +
-            "A tool that given a file, provides you with a\n" +
-            "serious of statistics of your choice.\n\n" +
-            "Start off by providing the file path you wish to process.\n" +
-            "If none is specified, a sample text is provided for you.\n"
-    );
+    System.out.println("\n*** Welcome to the FileAnalytics ***\n");
 
     // location for the sample file
     File sample = new File(System.getProperty("user.dir"), "resources/sample.txt");
@@ -45,23 +39,37 @@ public class RunFileAnalytics {
       System.out.println();
       System.out.println(
           "Choose your option:\n" +
-              "\t1) show file content\n" +
-              "\t2) count word occurrences\n" +
-              "\t3) count character occurrences per word\n" +
-              "\t4) additional information\n" +
-              "\t5) exit"
+              "\t1) show instructions\n" +
+              "\t2) show file content\n" +
+              "\t3) count word occurrences\n" +
+              "\t4) count character occurrences per word\n" +
+              "\t5) additional information\n" +
+              "\t6) exit"
       );
-      System.out.print("Your option [1-5]: ");
+      System.out.print("Your option [1-6]: ");
       option = in.nextInt();
 
       // validate user selection
       switch (option) {
         case 1:
+          System.out.println(
+              "\nFileAnalytics is a program that provides insights over a given file.\n" +
+                  "\n" +
+                  "A valid path file is required so using absolute path is recommended.\n" +
+                  "A possible value could be ~/documents/my_file.txt.\n" +
+                  "If none is specified, a sample text is provided for you.\n" +
+                  "\n" +
+                  "Afterwards an options menu is displayed.\n" +
+                  "Input the right option for the desired action.\n" +
+                  "For instance, to display these instruction, input option 1.\n" +
+                  "To exit program, input option 6.");
+          break;
+        case 2:
           // show content
           System.out.println();
           System.out.print(processor.content);
           break;
-        case 2:
+        case 3:
           // read string input
           System.out.print("Word to search by: ");
           String word = in.next();
@@ -69,7 +77,7 @@ public class RunFileAnalytics {
           System.out.printf("\nThere are %d occurrences of word '%s'.\n",
               processor.wordOccurrences(word), word);
           break;
-        case 3:
+        case 4:
           // read char input
           System.out.print("Character to search by: ");
           char ch = in.next().charAt(0);
@@ -86,7 +94,7 @@ public class RunFileAnalytics {
             ));
           }
           break;
-        case 4:
+        case 5:
           System.out.println();
           System.out.printf("There are %d words.\n", processor.wordCount());
           System.out.printf("There are %d vowels.\n", processor.vowelsCount());
@@ -96,13 +104,13 @@ public class RunFileAnalytics {
           System.out.printf("There are %d repetitive words: %s\n",
               processor.getRepetitiveWords().size(), processor.getRepetitiveWords().toString());
           break;
-        case 5:
+        case 6:
           System.out.println();
           System.out.println("Bye.");
           break;
         default:
           System.out.println("Provide a valid option.");
       }
-    } while (option != 5);
+    } while (option != 6);
   }
 }
